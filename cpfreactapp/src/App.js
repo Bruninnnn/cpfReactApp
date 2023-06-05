b73a4f29c49a172f04e77fbb5b088b31b229ab91
Guilherme Miranda <74930707+zGuilhermeTM@users.noreply.github.com>
2023-06-05 13:40:25 -0300

    Update Register, Login e Controle

--- cpfreactapp/src/App.js
+++ cpfreactapp/src/App.js
@@ -1,9 +1,11 @@
 import { Login } from "@mui/icons-material";
 import Home from "./components/Home/Home";
 import FormLogin from "./components/Login/FormLogin";
+import FormRegister from "./components/Register/FormRegister";
 
 function App() {
-  return <FormLogin></FormLogin>;
+  //return <FormLogin></FormLogin>;
+  return <FormRegister></FormRegister>;
   //return <Home></Home>;
 }
 
