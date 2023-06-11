86cc6300b439c51bcb6a791a817e7ede122287a0
Bruno Renan Pich <brunorenanpichdev@gmail.com>
2023-06-11 14:01:37 -0300

    Adicionando exemplos de Function

--- cpfreactapp/src/App.js
+++ cpfreactapp/src/App.js
@@ -1,11 +1,8 @@
-import { Login } from "@mui/icons-material";
-import Home from "./components/Home/Home";
-import FormLogin from "./components/Login/FormLogin";
 import FormRegister from "./components/Register/FormRegister";
 
 function App() {
-  return<FormLogin></FormLogin>;
-  //return <FormRegister></FormRegister>;
+  /* return<FormLogin></FormLogin>; */
+  return <FormRegister></FormRegister>;
   //return <Home></Home>;
 }
 
