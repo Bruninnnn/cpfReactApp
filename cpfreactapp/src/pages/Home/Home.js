8b14161dd31b4a3b2d578d5afafc7c18133bff6d
Guilherme Miranda <man.wolf07@gmail.com>
2024-04-02 20:06:31 -0300

    Update Routes and Correct more Bugs to Tailwind
    Corrijindo alguns bugs que se encontravam no flex e no width do tailwind, adicionado um Routes com a funcionalidade Outlet

--- cpfreactapp/src/pages/Home/Home.js
+++ cpfreactapp/src/pages/Home/Home.js
@@ -11,12 +11,7 @@ import SideBar from "../../components/SideBar";
 import { Context } from "../../Context";
 import { Table } from "./Table";
 
-import {
-  MdAdd,
-  MdAttachMoney,
-  MdMoneyOff,
-  MdOutlineToll,
-} from "react-icons/md";
+import { MdAdd, MdAttachMoney, MdMoneyOff, MdOutlineToll } from "react-icons/md";
 import { DashBoardBalances } from "./DashBoardBalances";
 
 function Home() {
@@ -88,12 +83,12 @@ function Home() {
     rowToEdit === null
       ? setRows([...rows, newRow])
       : setRows(
-          rows.map((currRow, idx) => {
-            if (idx !== rowToEdit) return currRow;
+        rows.map((currRow, idx) => {
+          if (idx !== rowToEdit) return currRow;
 
-            return newRow;
-          })
-        );
+          return newRow;
+        })
+      );
   };
 
   const [receipt, setReceipt] = useState();
@@ -231,18 +226,11 @@ function Home() {
   }, [rows]);
 
   return (
-    <div className="flex flex-row w-screen h-dvh bg-color-background text-color-border-login overflow">
-      {" "}
-      {/* body */}
-      <SideBar />
-      <div className="flex w-full h-full mx-4 my-0 justify-center">
-        {" "}
-        {/* container */}
-        <main className="ml-24 mt-8">
+    <div className="flex flex-row bg-color-background text-color-border-login overflow"> {/* body */}
+      <div className="flex w-full h-full mx-4 my-0 justify-center"> {/* container */}
+        <main className="mx-0 mt-8">
           <h1 className="mb-4">Registros</h1>
-          <div className="inline-block mt-4 rounded-[2rem]">
-            {" "}
-            {/* month */}
+          <div className="inline-block mt-4 rounded-[2rem]"> {/* month */}
             <input
               id="calendar"
               type="month"
@@ -251,60 +239,35 @@ function Home() {
               className="bg-color-rows text-sm text-[#ffffff] border-2 border-[solid] border-color-border p-2 text-center [transition:all_450ms_ease] hover:[transition:all_450ms_ease] hover:[box-shadow:none]"
             />
           </div>
-          <div className="flex m-lg:flex-row m-sm:m-md:flex-col w-full m-xl:w-[95%] h-1/4 m-md:h-3/4 m-lg:h-1/4 mt-10 m-md:gap-8 m-lg:gap-8 m-xl:gap-40 m-2xl:gap-56">
-            {" "}
-            {/* balances */}
-            <div className="bg-color-rows p-4 w-[15vw] m-md:w-full m-lg:w-[22vw] m-xl:w-[20vw] h-[18vh] rounded-3xl mt-2 border border-[solid] border-color-border [transition:all_300ms_ease]">
-              {" "}
-              {/* receipt */}
+          <div className="flex m-lg:flex-row m-sm:m-md:flex-col w-full m-xl:w-full h-1/4 m-md:h-3/4 m-lg:h-1/4 mt-10 m-md:gap-8 m-lg:gap-8 m-xl:gap-40 m-2xl:gap-72"> {/* balances */}
+            <div className="bg-color-rows p-4 w-[15vw] m-md:w-full m-lg:w-[22vw] m-xl:w-[20vw] h-[18vh] rounded-3xl mt-2 border border-[solid] border-color-border [transition:all_300ms_ease]"> {/* receipt */}
               <span>
-                <MdAttachMoney
-                  style={{ fontSize: "2.5rem" }}
-                  className="bg-color-receipt justify-center text-center p-1.5 mb-2 rounded-full"
-                />
+                <MdAttachMoney style={{ fontSize: '2.5rem' }} className="bg-color-receipt justify-center text-center p-1.5 mb-2 rounded-full" />
               </span>
-              <div className="middle">
-                {" "}
-                {/* middle */}
-                <h3 className="mb-4 text-[1rem] mt-1">Receita</h3>
-                <h1>{receipt}</h1>
+              <div className="middle"> {/* middle */}
+                
+                  <h3 className="mb-4 text-[1rem] mt-1">Receita</h3>
+                  <h1>{receipt}</h1>
+                
               </div>
             </div>
-            <div className="bg-color-rows p-4 w-[15vw] m-md:w-full m-lg:w-[22vw] m-xl:w-[20vw] h-[18vh] rounded-3xl mt-2 border border-[solid] border-color-border [transition:all_300ms_ease]">
-              {" "}
-              {/* balance */}
+            <div className="bg-color-rows p-4 w-[15vw] m-md:w-full m-lg:w-[22vw] m-xl:w-[20vw] h-[18vh] rounded-3xl mt-2 border border-[solid] border-color-border [transition:all_300ms_ease]"> {/* balance */}
               <span>
-                <MdOutlineToll
-                  style={{ fontSize: "2.5rem" }}
-                  className="bg-color-bginputs justify-center text-center p-1.5 mb-2 rounded-full"
-                />
+                <MdOutlineToll style={{ fontSize: '2.5rem' }} className="bg-color-bginputs justify-center text-center p-1.5 mb-2 rounded-full" />
               </span>
-              <div className="middle">
-                {" "}
-                {/* middle */}
-                <div className="left">
-                  {" "}
-                  {/* left */}
+              <div className="middle"> {/* middle */}
+                <div className="left"> {/* left */}
                   <h3 className="mb-4 text-[1rem] mt-1">Saldo</h3>
                   <h1>{balance}</h1>
                 </div>
               </div>
             </div>
-            <div className="bg-color-rows p-4 w-[15vw] m-md:w-full m-lg:w-[22vw] m-xl:w-[20vw] h-[18vh] rounded-3xl mt-2 border-[1px] border-[solid] border-color-border [transition:all_300ms_ease]">
-              {" "}
-              {/* cost */}
+            <div className="bg-color-rows p-4 w-[15vw] m-md:w-full m-lg:w-[22vw] m-xl:w-[20vw] h-[18vh] rounded-3xl mt-2 border-[1px] border-[solid] border-color-border [transition:all_300ms_ease]"> {/* cost */}
               <span>
-                <MdMoneyOff
-                  style={{ fontSize: "2.5rem" }}
-                  className="bg-color-cost justify-center text-center p-1.5 mb-2 rounded-full"
-                />
+                <MdMoneyOff style={{ fontSize: '2.5rem' }} className="bg-color-cost justify-center text-center p-1.5 mb-2 rounded-full" />
               </span>
-              <div className="middle">
-                {" "}
-                {/* middle */}
-                <div className="left">
-                  {" "}
-                  {/* left */}
+              <div className="middle"> {/* middle */}
+                <div className="left"> {/* left */}
                   <h3 className="mb-4 text-[1rem] mt-1">Despesas</h3>
                   <h1>{cost}</h1>
                 </div>
@@ -332,9 +295,7 @@ function Home() {
               userContext={userContext}
             />
           )}
-          <div className="flex justify-end mt-14 pr-0 pb-2">
-            {" "}
-            {/* btnDiv */}
+          <div className="flex justify-end mt-14 pr-0 pb-2"> {/* btnDiv */}
             <button
               className="bg-color-receipt h-8 w-16 rounded-2xl border-b-2 border-solid border-color-border text-[#ffffff]" /* darkButton */
               onClick={() => setModalAddOpen(true)}
