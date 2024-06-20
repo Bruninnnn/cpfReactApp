c883d726db273d291d66bba39be3d1bde6bbcbe0
Guilherme Miranda <man.wolf07@gmail.com>
2024-06-20 20:36:39 -0300

    Update index.jsx

--- cpfreactapp/src/routes/index.jsx
+++ cpfreactapp/src/routes/index.jsx
@@ -8,7 +8,7 @@ import Reports from "../pages/Reports/Reports";
 import Goals from "../pages/Goals/Goals";
 import BankAccount from "../pages/BankAccount/BankAccount";
 import RegisterForm from "../pages/Register/RegisterForm";
-import GoalsDetails from "../pages/Goals/GoalsDetails";
+import { GoalsDetails } from "../pages/Goals/GoalsDetails";
 
 import { Context } from "../Context";
 
